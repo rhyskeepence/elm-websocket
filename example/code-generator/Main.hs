49dc68c99b683b741d9c9d095ea0e9bded2bc73c
Rhys Keepence <rhys@rhyskeepence.com>
2017-10-22 16:27:56 +0100

    build the api for a task management application

--- example/code-generator/Main.hs
+++ example/code-generator/Main.hs
@@ -9,8 +9,14 @@ import Elm.Export
 spec :: Spec
 spec =
   moduleSpec ["Api"] $ do
+    renderType (Proxy :: Proxy TaskStatus)
+    renderType (Proxy :: Proxy Task)
     renderType (Proxy :: Proxy Message)
+    renderEncoder (Proxy :: Proxy TaskStatus)
+    renderEncoder (Proxy :: Proxy Task)
     renderEncoder (Proxy :: Proxy Message)
+    renderDecoder (Proxy :: Proxy TaskStatus)
+    renderDecoder (Proxy :: Proxy Task)
     renderDecoder (Proxy :: Proxy Message)
     renderSubscriber
 
--- example/server/shared/Api.hs
+++ example/server/shared/Api.hs
@@ -6,10 +6,38 @@ module Api where
 import Elm.Export
 import           GHC.Generics                  (Generic)
 import           Data.Aeson                    (FromJSON, ToJSON)
+import Data.Text
 
-data Message = SparklineUpdate
-  { newValue :: Int
+
+data TaskStatus
+  = Ready
+  | InPlay
+  | Done
+  deriving (Eq, Show, Generic, ElmType)
+
+instance ToJSON TaskStatus
+
+instance FromJSON TaskStatus
+
+data Task
+  = Task
+  { id :: Int
+  , name :: Text
+  , description :: Text
+  , status :: TaskStatus
   } deriving (Eq, Show, Generic, ElmType)
+
+instance ToJSON Task
+
+instance FromJSON Task
+
+
+data Message
+  = CreateTaskRequest Task
+  | LoadAllTasksRequest
+  | LoadAllTasksResponse [Task]
+  deriving (Eq, Show, Generic, ElmType)
+
 
 instance ToJSON Message
 
